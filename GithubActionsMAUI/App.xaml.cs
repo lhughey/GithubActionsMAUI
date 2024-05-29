dffd471d516ff4321feea652c6c5c797b61ade2e
Lyndon Hughey <lhughey@gmail.com>
2024-05-29 10:12:53 -0500

    init

---
+++ GithubActionsMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GithubActionsMAUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ GithubActionsMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GithubActionsMAUI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
